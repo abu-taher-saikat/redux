56ceca08e68b22e8da45c32b5217567f3c66e2b1
Abu Taher Saikat <abutahersaikat@pop-os.localdomain>
2020-07-11 00:29:53 +0600

    hello dear

---
+++ src/components/Saikat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import React, { Component } from 'react'
+
+export default class Saikat extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is saikat</h1>
+            </div>
+        )
+    }
+}
